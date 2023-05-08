bf63a9ff32ee84c52979b5984f15afa0e7c18620
madhesh kumar <madheshkumars03@gmail.com>
2023-05-08 21:31:17 +0530

    update complete with mongoDB

--- app.js
+++ app.js
@@ -4,54 +4,69 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var posts=[];
+mongoose
+  .connect("mongodb://127.0.0.1:27017/BlogDB")
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((err) => console.log("mongo error", err));
 
-app.get("/",function(req,res){
-  res.render("home",{StartingContent:homeStartingContent,PostContent:posts})
-});
-app.get("/about",function(req,res){
-  res.render("about",{AboutContent:aboutContent})
-});
-app.get("/contact",function(req,res){
-  res.render("contact",{ContactContent:contactContent})
-});
-app.get("/compose",function(req,res){
-  res.render("compose");
-});
-app.post("/compose",function(req,res){
-  const post={
-    postTitle:req.body.title,
-    postBody:req.body.post
-  };
-  posts.push(post);
-  res.redirect("/");
-})
+async function run() {
+  try {
+    const postSchema = new mongoose.Schema({
+      title: String,
+      content: String,
+    });
+    var Post = mongoose.model("post", postSchema);
+  } catch (err) {
+    console.log(err);
+  }
+  app.get("/", async function (req, res) {
+    var posts = await Post.find({});
+    res.render("home", {
+      StartingContent: homeStartingContent,
+      PostContent: posts,
+    });
+  });
+  app.get("/about", function (req, res) {
+    res.render("about", { AboutContent: aboutContent });
+  });
+  app.get("/contact", function (req, res) {
+    res.render("contact", { ContactContent: contactContent });
+  });
+  app.get("/compose", function (req, res) {
+    res.render("compose");
+  });
+  app.post("/compose", async function (req, res) {
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.post,
+    });
+    await post.save();
 
+    res.redirect("/");
+  });
 
-app.get("/posts/:param",function(req,res){
-  const reqpage=_.lowerCase(req.params.param);
-  posts.forEach(function(post)
-  {
-    const storedPage=_.lowerCase(post.postTitle);
-    if (reqpage === storedPage) 
-    {
-      res.render("post",{Title:post.postTitle,Content:post.postBody});
-    }
-  })
-})
+  app.get("/posts/:postId", async function (req, res) {
+    const reqpostId = req.params.postId;
+    const reqpost = await Post.findOne({_id:reqpostId});
+    res.render("post", { Title: reqpost.title,Content: reqpost.content });
+  });
+}
+run().catch(console.dir);
 
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
